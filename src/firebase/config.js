b7e5243037102d7fd97b52b87be81c3b56b890ab
Ulises Huber <75042766+Ulisesx21@users.noreply.github.com>
2023-08-22 17:47:13 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyABg5uPCsDnuCfbRoonitA_ouguphqXLdQ",
+  authDomain: "todo-29a73.firebaseapp.com",
+  projectId: "todo-29a73",
+  storageBucket: "todo-29a73.appspot.com",
+  messagingSenderId: "834512189239",
+  appId: "1:834512189239:web:433eac4169bb8d0657ccb2",
+  measurementId: "G-N4XT8B8PE3",
 };
 
 const app = initializeApp(firebaseConfig);
